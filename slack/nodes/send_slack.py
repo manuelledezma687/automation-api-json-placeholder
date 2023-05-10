512dde9eff131e26eb866ca2761909aac2443568
Manuel Ledezma <92046228+manuelledezma687@users.noreply.github.com>
2023-05-09 21:11:59 -0300

    Update send_slack.py

--- slack/nodes/send_slack.py
+++ slack/nodes/send_slack.py
@@ -9,7 +9,7 @@ class SendSlack:
     def send_slack(self,message_slack):
         header = {
             "Content-Type": "application/json",
-            "Authorization": "Bearer xoxb-5227628382162-5228706929107-Nunf8k3RN49fnCH2ppQ3n8G7"
+            "Authorization": "Bearer XXXXXXX"
         }
         response = requests.post(url=SLACK_URL, headers=header, data=json.dumps(message_slack))
         if response.ok:
